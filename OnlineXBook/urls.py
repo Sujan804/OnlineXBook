9b81b10987ac0591d14fcb42578028000afba334
Sujan804 <sujanmec804@gmail.com>
2022-07-31 00:11:10 +0600

    store portion completed

--- OnlineXBook/urls.py
+++ OnlineXBook/urls.py
@@ -20,5 +20,5 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('store/',include('store.urls'))
+    path('',include('store.urls')),
 ]
--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,13 @@
-import imp
 from django.urls import path 
 from . import views
 app_name = 'store'
 urlpatterns = [
-    
+    path('',views.index,name='index'),
+    path('login',views.login,name='login'),
+    path('logout',views.signout,name = 'signout'),
+    path('registration',views.registration,name='registration'),
+    path('book/<int:id>',views.get_book,name='book'),
+    path('books',views.get_books, name='books'),
+    path('category/<int:id>',views.get_book_category, name='category'),
+    path('writer/<int:id>',views.get_writer,name='writer')    
 ]
